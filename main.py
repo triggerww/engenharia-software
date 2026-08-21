4f3aed8337f8cafb4b6740f1231dce30cc02271a
Trigger <triggerww@outlook.com>
2026-08-21 16:07:28 -0300

    nova branch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Olá, Mundo!")
+import sys
 
+def main(argv):
+    if len(argv) > 1:
+        print(f'Olá {argv[1]}!')
+    else:
+        print('Olá Mundo!')
 
-if __name__  == '__main__':
-    main()
+if __name__ == '__main__':
+    main(sys.argv)
