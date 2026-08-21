8a1e10c6122db15ad1bf5112b43434e709667897
Trigger <triggerww@outlook.com>
2026-08-21 16:29:36 -0300

    implementei com argparse

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import argparse
+
 def main():
-    print("Olá, Mundo!")
+    parser = argparse.ArgumentParser(
+        description='Cumprimenta alguém.'
+    )
+    parser.add_argument(
+        "nome",
+        nargs='?',
+        default='mundo',
+        help='Nome da pessoa a ser cumprimentada'
+    )
+    args = parser.parse_args()
+    print(f'Olá {args.nome}!')
 
-
-if __name__  == '__main__':
+if __name__ == '__main__':
     main()
